8e03e423f2009810bd4759f23d3ed77562aa5ec7
ryxwaer <ryxwaer@gmail.com>
2024-04-08 20:11:36 +0200

    bugfixes

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // Check for a completed navigation in a tab that matches your target URL
-  if (changeInfo.status === 'complete' && tab.url.match(/https:\/\/.*\.hana\.ondemand.com\/shell\/design\/contentpackage\/.*/)) {
+  if (changeInfo.status === 'complete' && tab.url && tab.url.match(/https:\/\/.*\.hana\.ondemand.com\/shell\/design\/contentpackage\/.*/)) {
     // Inject the content script into the tab
     chrome.scripting.executeScript({
       target: { tabId: tabId },
--- content.js
+++ content.js
@@ -1,18 +1,33 @@
 (function() {
+
     // First, attempt to revert any previous hiding for robustness
     const previouslyHidden = document.querySelectorAll('.sapTntToolPageAside');
     previouslyHidden.forEach(el => {
         el.style.display = ''; // Remove the display style to revert to default
     });
 
-    // Check if the current page matches your specific criteria
-    if (window.location.href.includes("/integrationflows/")) {
-        const elementsToHide = document.querySelectorAll('.sapTntToolPageAside');
-        elementsToHide.forEach(el => {
-            el.style.display = 'none'; // Hide element
-        });
-        if (elementsToHide.length > 0) {
-            console.log(`${elementsToHide.length} elements hidden.`);
+    // Function to hide elements, called directly and by the observer when needed
+    function hideElements() {
+        if (window.location.href.includes("/integrationflows/")) {
+            const elementsToHide = document.querySelectorAll('.sapTntToolPageAside');
+            elementsToHide.forEach(el => {
+                el.style.display = 'none'; // Hide element
+            });
+            if (elementsToHide.length > 0) {
+                console.log(`${elementsToHide.length} elements hidden.`);
+                observer.disconnect();
+            }
         }
     }
+
+    // Observer to watch for DOM changes and apply hiding logic as needed
+    const observer = new MutationObserver((mutations) => {
+        hideElements();
+    });
+
+    // Start observing the document body for added nodes and subtree modifications
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Initial call to attempt to hide elements before observer kicks in
+    hideElements();
 })();
